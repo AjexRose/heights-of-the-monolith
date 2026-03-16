65c00ced27aa0eb33cf5054e436fde7048750386
Alexander Carrizales <alexcarrizales554@gmail.com>
2026-03-16 14:30:08 -0500

    Adds credits to the prototype

--- Scripts/Menus/Main_Menu.gd
+++ Scripts/Menus/Main_Menu.gd
@@ -8,3 +8,6 @@ func _on_settings_btn_pressed() -> void:
 
 func _on_quit_bun_pressed() -> void:
 	get_tree().quit()
+
+func _on_credits_btn_pressed() -> void:
+	get_tree().change_scene_to_file("res://Menus/credits_menu.tscn")
